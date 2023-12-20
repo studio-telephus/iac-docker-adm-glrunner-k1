d7c419116b2ac62de05fd79f34cc722a0fb92c91
Kristo Aun <kristoaun@live.com>
2023-12-20 15:29:12 +0200

    run

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "container_glrunner_k1" {
-  source    = "github.com/studio-telephus/tel-iac-modules-lxd.git//instance?ref=develop"
+  source    = "github.com/studio-telephus/terraform-lxd-instance.git?ref=main"
   name      = "container-glrunner-k1"
   image     = "images:debian/bookworm"
   profiles  = ["limits", "fs-dir", "nw-adm"]
@@ -16,14 +16,14 @@ module "container_glrunner_k1" {
     "${path.cwd}/filesystem-shared-ca-certificates",
     "${path.cwd}/filesystem",
   ]
-  exec = {
-    enabled     = true
-    entrypoint  = "/mnt/install.sh"
+  exec_enabled = true
+  exec = [{
+    entrypoint = "/mnt/install.sh"
     environment = {
       RANDOM_STRING                  = "d4a101e8-e4c1-4382-a9db-c63bdac6b773"
       GITLAB_RUNNER_REGISTRATION_KEY = var.gitlab_runner_registration_key
       GIT_SA_USERNAME                = var.git_sa_username
       GIT_SA_TOKEN                   = var.git_sa_token
     }
-  }
+  }]
 }
